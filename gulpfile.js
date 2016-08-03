0c3b2641937392b9602e5e226a502ea61d6dc762
SixGod <41421669@qq.com>
2016-08-03 12:02:01 +0800

    hehe

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,7 @@
 var gulp = require('gulp');
-var uglifyjs = require('gulp-uglify');
-var buffer = require('vinyl-buffer');
-var less = require('gulp-less');
-var babelify = require('babelify');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
 var minifycss = require('gulp-minify-css');
-var webserver = require("gulp-webserver");
-var concat = require('gulp-concat');
+var webserver = require('gulp-webserver');
+var less = require('gulp-less');
 var del = require('del');
 
 
@@ -15,21 +9,8 @@ var del = require('del');
 gulp.task('minifyless', function () {
     gulp.src('./app/assets/less/app.less')
         .pipe(less())
-        //.pipe(minifycss())
         .pipe(gulp.dest('./app/assets/minicss'));
 });
-
-///* 合并js依赖 */
-//gulp.task('browserify', function() {
-//    browserify('./app/index/src/app.js')
-//        .transform(babelify)
-//        .bundle()
-//        .on('error',function(err){console.log(err.message)})
-//        .pipe(source('bundle.js'))
-//        //.pipe(buffer())
-//        //.pipe(uglifyjs())
-//        .pipe(gulp.dest('./app/index/build'));
-//});
 
 gulp.task('webserver', function() {
     gulp.src('./app')
@@ -48,9 +29,5 @@ gulp.task("cleancss",function(cb){
 gulp.task("watcherless",function(){
     gulp.watch(['./app/assets/less/**/*.less'],['cleancss','minifyless']);
 });
-
-//gulp.task("watcherjs",function(){
-//    gulp.watch('./app/index/src/**/*.js',['cleanjs','browserify']);
-//});
 
 gulp.task('default',['minifyless','watcherless','webserver']);
